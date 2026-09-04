69496c307bd1ee0188081fe2015dfe1632ea1b0d
Jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-03 21:50:06 -0300

    9

---
+++ Lista1/exercicio9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 9</title>
+</head>
+<body>
+    <h1>Exercício de Exemplo</h1>
+    <form method="post" action="exercicio9.php">
+        <label for="largura"> Informe a largura do retângulo:</label>
+        <input type="number" id="largura" name="largura">
+	<label for="altura"> Informe a altura do retângulo:</label>
+        <input type="number" id="altura" name="altura">
+        <button type="submit">Enviar</button>
+    </form>
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $largura = $_POST["largura"];
+            $altura = $_POST["altura"];
+            $perimetroretangulo = ($largura + $altura) * 2;
+            echo "O perímetro do retângulo é: $perimetroretangulo";
+        }
+    ?>
+</body>
+</html>
+
+
+    
