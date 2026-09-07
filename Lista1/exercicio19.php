a39253c0bcf438dceadd806686e7e3109a158f83
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-07 09:56:31 -0300

    exerc19

---
+++ Lista1/exercicio19.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 19</title>
+</head>
+<body>
+    <h1>Exercício de Exemplo</h1>
+    <form method="post" action="exercicio19.php">
+        <label for="distancia"> Informe a distância em km :</label>
+        <input type="numero" id="distancia" name="distancia">
+	<label for="tempo"> Informe o tempo percorrido em horas:</label>
+        <input type="number" id="tempo" name="tempo">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+    		$distancia = $_POST["distancia"];
+    		$tempo = $_POST["tempo"];
+   		    $velocidademedia = $distancia / $tempo;
+    		echo "O resultado da veocidade média é: $velocidademedia";
+        }
+?>
