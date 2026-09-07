d138bc0dbef7da0737cc27829324c8ae6b300311
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-07 09:11:20 -0300

    exerc16

---
+++ Lista1/exercicio16.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 16</title>
+</head>
+<body>
+    <h1>Exercício de Exemplo</h1>
+    <form method="post" action="exercicio16.php">
+        <label for="capital"> Informe o capital :</label>
+        <input type="number" id="capital" name="capital" step="0.01" min="0">
+	<label for="taxa"> Informe a taxa de juros:</label>
+        <input type="number" id="taxa" name="taxa">
+        <label for="periodo"> Informe o período em meses:</label>
+        <input type="number" id="periodo" name="periodo">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $capital = $_POST["capital"];
+            $taxa = $_POST["taxa"];
+            $periodo = $_POST["periodo"];
+            $juros = $capital *($taxa / 100) * $periodo;
+            echo "O resultado dos juros simples sobre o capital é: $juros";
+        }
+?>
