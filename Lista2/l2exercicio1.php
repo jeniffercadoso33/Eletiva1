e25b77b716f4e9d4c8d1976df835f379b0145f43
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-07 09:27:44 -0300

    novoexer1l2

--- Lista2/l2exercicio1.php
+++ Lista2/l2exercicio1.php
@@ -1,46 +1,46 @@
-<!--Exercício - Verificar se um valor é igual ou diferente de 10 -->
-<!--https://vanessaborges2.github.io/Gerador-Formulario/-->
 <!doctype html>
 <html lang="pt-BR">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercício Seleção 1</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Estrutura Condicional IF e SWITCH</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
-    <body> 
-        <div class="container py-3">
-            <h1>Estrutura Condicional - Exercício 1</h1>
-            <form method="post" action="l2exercicio1.php">
-                <div class="mb-3">
-                <label for="valor1" class="form-label">Informe o valor 1</label>
-                <input type="number" id="valor1" name="valor1" class="form-control" required="">
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-        
-        <?php
+<body> 
+<div class="container py-3">
+<h1>Estrutura Condicional IF e SWITCH</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor" class="form-label">Informe o valor</label>
+              <input type="number" id="valor" name="valor" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">Informe outro valor</label>
+              <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+<?php
             
-            if($_SERVER['REQUEST_METHOD'] == 'POST')
-              {
+    if($_POST)
+        {
 
-                      $valor1 = $_POST['valor1'];
-                      $valor2 = $_POST['valor2'];
+            $valor = $_POST['valor'];
+            $valor2 = $_POST['valor2'];
 
-
-                      if($valor1 == $valor2)
-                        {
-                         $triplo = ($valor1 + $valor2) * 3;
+                if($valor == $valor2)
+                    {
+                         $triplo = ($valor + $valor2) * 3;
                          echo " <p> O triplo da soma dos números é: $triplo <p>";         
-                        }
+                    }
 
-                      else
-                          echo "<p> Valores são diferentes <p>";
+                else
+                    echo "<p> Valores são diferentes <p>";
 
-                }
-        ?>
-        </div>
-        
-    </body>
+        }
+
+?>
+</div>
+</body>
 </html>
