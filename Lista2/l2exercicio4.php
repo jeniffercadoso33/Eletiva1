5f04b498ae693633cd485dd20f5ece58d9571205
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-07 09:37:19 -0300

    ex4

---
+++ Lista2/l2exercicio4.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Estrutura Condicional IF e SWITCH</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Estrutura Condicional IF e SWITCH</h1>
+<form method="post" action="l2exercicio4.php">
+<div class="mb-3">
+              <label for="valormes" class="form-label">Informe um numero de 1 a 12</label>
+              <input type="number" id="valormes" name="valormes" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            switch($valormes)
+                {
+                    case 1:
+                        echo "Janeiro";
+                        break;
+                    case 2:
+                        echo "Fevereiro";
+                        break;
+                    case 3:
+                        echo "Março";
+                        break;
+					case 4:
+						echo "Abril";
+					    break;
+					case 5:
+						echo "Maio";
+					    break;
+					case 6:
+						echo "Junho";
+					    break;
+					case 7:
+						echo "Julho";
+					    break;
+					case 8:
+					echo "Agosto";
+					    break;
+					case 9:
+						echo "Setembro";
+					    break;
+					case 10:
+						echo "Outubro";
+					    break;	
+					case 11:
+						echo "Novembro";
+					    break;
+					case 12:
+						echo "Dezembro";
+					    break;
+                    default:
+                        echo "Valor diferente de 1, 2 ou 3";
+                        break;
+               }
+        }
+        ?>
+
+</div>
+</body>
+</html>
