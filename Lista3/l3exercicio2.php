a106c84339adccb433cbfeb92bba417d7c04b483
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-12 13:43:56 -0300

    2

---
+++ Lista3/l3exercicio2.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post"  action="l3exercicio2.php">
+<div class="mb-3">
+              <label for="palavra" class="form-label">Informe uma palavra</label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $palavra = $_POST['palavra'] ?? "";
+            if($palavra != "" )
+                {
+                    echo "<p> Trabalhando maíusculas e minísculas </p>";
+                    $ma = strtoupper($palavra);
+                    echo "<p>: $ma </p>";
+                    $mi = strtolower($palavra);
+                    echo "<p>: $mi </p>";
+
+                    }
+
+        }
+
+    
+
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
