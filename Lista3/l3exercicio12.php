dcb124ce393de1c7243e8685014b451d623d4345
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-13 14:12:15 -0300

    12

---
+++ Lista3/l3exercicio12.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Funções PHP</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Funções PHP</h1>
+<form method="post" action="l3exercicio12.php">
+<div class="mb-3">
+              <label for="senha" class="form-label">Informe a senha</label>
+              <input type="password" id="senha" name="senha" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
