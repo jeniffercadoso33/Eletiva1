c891457f6ae04eb3d56aa25cb7ed5f0ef8628a08
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-13 13:17:08 -0300

    1

--- Lista3/l3exercicio1.php
+++ Lista3/l3exercicio1.php
@@ -8,7 +8,7 @@
 </head>
 <body> 
 <div class="container py-3">
-<h1></h1>
+<h1>Funções PHP</h1>
 <form method="post"  action="l3exercicio1.php">
 <div class="mb-3">
               <label for="palavra" class="form-label">Informe uma palavra</label>
