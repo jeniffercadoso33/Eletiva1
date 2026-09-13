fedd7d31923881d95b3fff3d4cafca1cce7f6d74
jeniffer <jeniffer.silva@aluno.cps.sp.gov.br>
2026-09-13 15:49:31 -0300

    12

--- Lista3/l3exercicio12.php
+++ Lista3/l3exercicio12.php
@@ -11,11 +11,47 @@
 <h1>Funções PHP</h1>
 <form method="post" action="l3exercicio12.php">
 <div class="mb-3">
-              <label for="senha" class="form-label">Informe a senha</label>
-              <input type="password" id="senha" name="senha" class="form-control" required="">
+              <label for="resposta" class="form-label">Deseja criar uma senha ( digite sim ou não)</label>
+              <input type="text" id="resposta" name="resposta" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+      {
+        $resposta = mb_strtolower(trim(filter_input(INPUT_POST, 'resposta', FILTER_DEFAULT) ?? ''));
+        if ($resposta === 'sim') 
+          {    
+            $letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $numeros = '0123456789';
+
+        
+            $sorteioLetras = '';
+            for ($i = 0; $i < 4; $i++) 
+              {
+                $sorteioLetras .= $letras[rand(0, strlen($letras) - 1)];
+              }
+            $sorteioNumeros = '';
+            for ($i = 0; $i < 4; $i++) 
+              {
+                $sorteioNumeros .= $numeros[rand(0, strlen($numeros) - 1)];
+              }
+            $senha = $sorteioLetras . $sorteioNumeros;
+
+            echo "<div class='alert alert-success mt-3'>";
+            echo "Senha gerada: " . htmlspecialchars($senha, ENT_QUOTES, 'UTF-8');
+            echo "</div>";
+          }
+          else 
+            {
+              echo "<div class='alert alert-secondary mt-3'>";
+              echo "Nenhuma senha foi gerada.";
+              echo "</div>";
+            }
+    }
+    ?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
